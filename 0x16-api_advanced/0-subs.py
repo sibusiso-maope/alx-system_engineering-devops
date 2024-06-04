986ebab351bb2d4c5d9b489ee9cc6636a3b657cb
sibusiso-maope <Sibusiso.Maope@gmail.com>
2024-06-04 06:31:06 -0700

    MY task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,20 +1,27 @@
 #!/usr/bin/python3
 """
-Queries the Reddit API and returns the number of total subscribers for a given
-subreddit.
+number of subscribers for a given subreddit
 """
-import requests
+
+from requests import get
 
 
 def number_of_subscribers(subreddit):
     """
-    Queries the Reddit API and returns the number of total subscribers for a
-    given subreddit.
+    function that queries the Reddit API and returns the number of subscribers
+    (not active users, total subscribers) for a given subreddit.
     """
-    url = 'http://www.reddit.com/r/{}/about.json'.format(subreddit)
-    headers = {"User-Agent": "Mozilla/10.0/API)"}
-    response = requests.get(url, headers=headers)
-    if (not response.ok):
+
+    if subreddit is None or not isinstance(subreddit, str):
         return 0
-    subscriber_count = response.json().get('data').get('subscribers')
-    return subscriber_count
+
+    user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    response = get(url, headers=user_agent)
+    results = response.json()
+
+    try:
+        return results.get('data').get('subscribers')
+
+    except Exception:
+        return 0
